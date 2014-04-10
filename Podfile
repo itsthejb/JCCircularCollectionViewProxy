bfa17356295500b2ad450d63dc13090311e17f79
Jonathan Crooke <joncrooke@gmail.com>
2014-04-10 22:16:23 +0200

    Some specs

--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ platform :ios, '6.0'
 target :Tests, :exclusive => true do
 pod 'Expecta'
 pod 'Specta'
+pod 'OCMock'
 end
 
 inhibit_all_warnings!
--- Tests/Specs.m
+++ Tests/Specs.m
@@ -1,34 +1,44 @@
 //
-//  JCCircularCollectionViewProxyTests.m
-//  JCCircularCollectionViewProxyTests
+//  Specs.m
+//  JCCircularCollectionViewProxy
 //
 //  Created by Jonathan Crooke on 10/04/2014.
 //  Copyright (c) 2014 Jonathan Crooke. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import "JCCircularCollectionViewProxy.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
+#import "Specta.h"
+#import "OCMock.h"
 
-@interface JCCircularCollectionViewProxyTests : XCTestCase
+SpecBegin(JCCircularCollectionViewProxy)
 
-@end
+__block UICollectionView *collectionView = nil;
+__block id mockDataSource = nil;
+__block id mockDelegate = nil;
+__block JCCircularCollectionViewProxy *proxy = nil;
 
-@implementation JCCircularCollectionViewProxyTests
+before(^{
+  mockDataSource = [OCMockObject mockForProtocol:@protocol(JCCircularCollectionViewProxyDataSource)];
+  mockDelegate = [OCMockObject mockForProtocol:@protocol(UICollectionViewDelegateFlowLayout)];
+  collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 0, 100, 200)
+                                      collectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+  [[[mockDataSource stub] andReturnValue:@1] numberOfSectionsInCollectionView:collectionView];
+  [[[mockDelegate stub] andReturnValue:[NSValue valueWithCGSize:CGSizeMake(50, 50)]]
+   collectionView:collectionView
+   layout:collectionView.collectionViewLayout
+   sizeForItemAtIndexPath:[OCMArg any]];
+  proxy = [collectionView circularProxyWithDataSource:mockDataSource
+                                             delegate:mockDelegate];
+});
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+describe(@"data source", ^{
+  it(@"should get number of items from true data source", ^{
+    [[[mockDataSource stub] andReturnValue:@99] collectionView:collectionView numberOfItemsInSection:0];
+    expect([proxy collectionView:collectionView numberOfItemsInSection:0]).to.equal(99 * 7000);
+  });
+});
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-@end
+SpecEnd
